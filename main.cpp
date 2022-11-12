fc88737af2eea49bec2f0e8c0f26984355928ecf
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2022-11-12 11:44:00 +0100

    podpięty sfml

--- main.cpp
+++ main.cpp
@@ -2,9 +2,21 @@
 #include <SFML/Graphics.hpp>
 int main()
 {
-    sf::Window oknoAplikacji( sf::VideoMode( 800, 600, 32 ), "Kurs SFML - http://cpp0x.pl" );
-    while( oknoAplikacji.isOpen() )
+    sf::Font font;
+    font.loadFromFile("arial.ttf");
+    sf::Text text;
+    text.setFont(font);
+    text.setString("Siema mati lubi natalke");
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::Black);
+    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    text.setPosition(250,270);
+    sf::RenderWindow oknoAplikacji( sf::VideoMode( 800, 600, 32 ), "Kurs SFML - http://cpp0x.pl" );
+    oknoAplikacji.setVerticalSyncEnabled(true);
+    while( oknoAplikacji.isOpen())
     {
+        oknoAplikacji.clear(sf::Color(sf::Color::White));
+        oknoAplikacji.draw(text);
         oknoAplikacji.display();
         sf::Event event;
         while (oknoAplikacji.pollEvent(event))
@@ -14,7 +26,6 @@ int main()
                 oknoAplikacji.close();
         }
     }
-std::cout<<"Siema mati lubi natalke"; //XD
     return 0;
 }
 
