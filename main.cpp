2c9d5faa65e5c93f546d6cbcfbeb4df0c6784c8a
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2022-11-12 11:21:48 +0100

    podpięty sfml

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 int main()
 {
+    sf::Window oknoAplikacji( sf::VideoMode( 800, 600, 32 ), "Kurs SFML - http://cpp0x.pl" );
+    while( oknoAplikacji.isOpen() )
+    {
+        oknoAplikacji.display();
+        sf::Event event;
+        while (oknoAplikacji.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                oknoAplikacji.close();
+        }
+    }
 std::cout<<"Siema mati lubi natalke"; //XD
     return 0;
 }
