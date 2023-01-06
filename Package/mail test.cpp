e184e7da6b29b3f69a41fc86782980057ecd9fdd
MTX <96997652+MTX77@users.noreply.github.com>
2023-01-06 23:36:46 +0100

    maile test1

---
+++ Package/mail test.cpp
@@ -0,0 +1,31 @@
+// Create a new instance of MailMessage class
+System::SharedPtr<MailMessage> message = System::MakeObject<MailMessage>();
+System::SharedPtr<SmtpClient> client = System::MakeObject<SmtpClient>();
+
+// Set subject of the message, Html body and sender information
+message->set_Subject(u"New message created by Aspose.Email for .NET");
+message->set_From(System::MakeObject<MailAddress>(L"from@domain.com", L"Sender Name", false));
+message->set_Body(System::String(u"<b>This line is in bold.</b> <br/> <br/>") + u"<font color=blue>This line is in blue color</font>");
+message->set_BodyEncoding(System::Text::Encoding::get_ASCII());
+// Add TO recipients and Add CC recipients
+message->get_To()->Add(System::MakeObject<MailAddress>(L"to1@domain.com", L"Recipient 1", false));
+message->get_To()->Add(System::MakeObject<MailAddress>(L"to2@domain.com", L"Recipient 2", false));
+message->get_CC()->Add(System::MakeObject<MailAddress>(L"cc1@domain.com", L"Recipient 3", false));
+
+// Specify your mailing host server, Username, Password, Port # and Security option
+client->set_Host(u"mail.server.com");
+client->set_Username(u"username");
+client->set_Password(u"password");
+client->set_Port(587);
+client->set_SecurityOptions(Aspose::Email::Clients::SecurityOptions::SSLExplicit);
+
+try
+{
+// Send this message
+client->Send(message);
+}
+catch (System::Exception& ex)
+{
+
+}
+
