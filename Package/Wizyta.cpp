5e6e36222b691dfea0d7f1d3d643d5ebb9fbacb5
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-01-10 17:12:19 +0100

    start kalendarz

---
+++ Package/Wizyta.cpp
@@ -0,0 +1,8 @@
+#include "../includes/Wizyta.h"
+
+Pacjent Wizyta::get_pacjent() {return pacjent;}
+
+std::string Wizyta::get_data() {return data;}
+
+std::string Wizyta::get_godzina() {return godzina;}
+
