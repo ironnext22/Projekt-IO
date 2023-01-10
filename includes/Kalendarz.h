3f0a07c69ee4ec0e94c5532b5f298309e831f7b5
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-01-10 19:45:39 +0100

    init comit kalendarz

--- includes/Kalendarz.h
+++ includes/Kalendarz.h
@@ -11,5 +11,10 @@ public:
     Kalendarz();
     void dodal_wizyte(std::string imie, std::string nazwisko, std::string pesel, std::string data, std::string godzina);
     std::vector<Wizyta> get_wizyty();
+    void set_imie();
+    void set_nazwisko();
+    void set_pesel();
+    void set_data();
+    void set_godzina();
 };
 #endif //PROJEKT_IO_KALENDARZ_H
