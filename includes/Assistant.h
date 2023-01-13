8b55582e07c9a2ab61ed56f1032f0f2e79c6a9ef
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-01-13 11:03:45 +0100

    kalendarz uprawnienia

--- includes/Assistant.h
+++ includes/Assistant.h
@@ -1,8 +1,11 @@
 #ifndef PROJEKT_IO_ASSISTANT_H
 #define PROJEKT_IO_ASSISTANT_H
 #include "Pracownik.h"
+#include "Kalendarz.h"
 class Assistant:public Pracownik
 {
-
+Kalendarz kalendarz;
+public:
+     Kalendarz get_kalendars();
 };
 #endif //PROJEKT_IO_ASSISTANT_H
