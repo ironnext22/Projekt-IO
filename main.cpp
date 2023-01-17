c8b2a1221c390e6079a4da65ab0455d32ea38278
MKuzera <mateusz.qzera@gmail.com>
2023-01-17 12:23:48 +0100

    idk

--- main.cpp
+++ main.cpp
@@ -6,18 +6,12 @@
 #include "includes/Assistant.h"
 #include "includes/Magazyn.h"
 #include "includes/Finanse.h"
-//Kalendarz test;
-Magazyn t2;
-Finanse test;
+
+
 using namespace sf;
 int main()
 {
-    //test.dodaj_tranzakcje("X1","19.01.2023","500");
-//    test.dodal_wizyte("Mateusz","Lugowski","23412343","19.01.2023","19.00","wiktor.k.2002@icloud.com");
-    //t2.dodaj_do_magazynu("Plomba","T20","200");
     Okno menu;
-
-
     while(menu.get_window_is_open()) {
 
                 menu.update();
