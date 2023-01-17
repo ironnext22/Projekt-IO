3beef19276b78eda30b77928357e630347be86dd
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-01-17 10:43:26 +0100

    ID fix

--- Package/Assistant.cpp
+++ Package/Assistant.cpp
@@ -1,3 +1,5 @@
 #include "../includes/Assistant.h"
 
 Kalendarz Assistant::get_kalendars() {return kalendarz;}
+
+Finanse Assistant::get_finanse() {return finanse;}
--- includes/Assistant.h
+++ includes/Assistant.h
@@ -2,11 +2,14 @@
 #define PROJEKT_IO_ASSISTANT_H
 #include "Pracownik.h"
 #include "Kalendarz.h"
+#include "Finanse.h"
 class Assistant:public Pracownik
 {
 Kalendarz kalendarz;
+Finanse finanse;
 public:
      Kalendarz get_kalendars();
      Assistant(std::string login,std::string haslo): Pracownik(login,haslo){}
+     Finanse get_finanse();
 };
 #endif //PROJEKT_IO_ASSISTANT_H
